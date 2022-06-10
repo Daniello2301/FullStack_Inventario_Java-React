b195d0f07f322d21e870e866fc61be93b772f2f7
Daniello2301 <daniel.lopera@est.iudigital.edu.co>
2022-06-09 21:42:02 -0500

    editin home page

--- frontend_inventario/src/views/HomePage.jsx
+++ frontend_inventario/src/views/HomePage.jsx
@@ -3,6 +3,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './HomePage.css';
 
 import IUD from '../assets/IUD.jpg';
+import IUD2 from '../assets/IUD2.png';
+import IUD3 from '../assets/IUD3.jpg';
 import { Sidebar } from '../components/sidebar/Sidebar';
 import {Footer} from '../components/footer/Footer';
 
@@ -12,31 +14,30 @@ export function HomePage(){
             <Sidebar />
             <main>
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active w-100 h-100">
-                        <img src={IUD} class="d-block w-100 h-100" alt="..."/>
-                        <a></a>
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="..."/>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={IUD}  alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={IUD2} alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={IUD3}  alt="..."/>
+                        </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="..."/>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </main>
             <Footer />
